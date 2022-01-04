10da65bc8ab46312c89094aca82d7101c40f5e4f
Francis Dolbec <fdolbec123@gmail.com>
2022-01-03 23:40:09 -0500

    Beginning of the layout for the different zones.

--- Color Palette/ContentView.swift
+++ Color Palette/ContentView.swift
@@ -9,8 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Color Palette")
-            .padding()
+        VStack{
+            HStack{
+                TextField("Recherche...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                    .frame(minWidth: 350)
+                    .padding(EdgeInsets(top: 20, leading: 20, bottom: 20, trailing: 10))
+                Button("Chercher") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }
+                    .frame(minWidth: 72)
+                    .padding(EdgeInsets(top: 20, leading: 10, bottom: 20, trailing: 20))
+            }
+            .padding(EdgeInsets(top: 30, leading: 10, bottom: 5, trailing: 10))
+            
+            HStack{
+                VStack{
+                    Text("Color Palette")
+                        .padding()
+                    Text("Color Palette")
+                        .padding()
+                    Text("Color Palette")
+                        .padding()
+                    Text("Color Palette")
+                        .padding()
+                    Text("Color Palette")
+                        .padding()
+                }
+                Text("Plus d'infos")
+                    .padding()
+            }
+        }
     }
 }
 
@@ -18,7 +46,5 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
             .frame(width: 1920.0, height: 1080.0)
-            
-            
     }
 }
