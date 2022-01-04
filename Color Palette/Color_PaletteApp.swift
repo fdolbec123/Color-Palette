d022e6fdf9c1177ef4d19cae22c67246d932d1a0
Francis Dolbec <fdolbec123@gmail.com>
2022-01-03 21:08:20 -0500

    Initial Commit

---
+++ Color Palette/Color_PaletteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Color_PaletteApp.swift
+//  Color Palette
+//
+//  Created by Francis Dolbec on 2022-01-03.
+//
+
+import SwiftUI
+
+@main
+struct Color_PaletteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
