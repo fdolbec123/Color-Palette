f0e4dacb74eadd366d0a07a4f0c7cdec41dac951
Francis Dolbec <fdolbec123@gmail.com>
2022-01-09 23:59:37 -0500

    Functionnal parser.
    Need to check how to store data.
    Need to find a way to parse parameters in categories (ex.: <couleur nom="Tirelire">)

--- Color Palette/Color_PaletteApp.swift
+++ Color Palette/Color_PaletteApp.swift
@@ -9,12 +9,22 @@ import SwiftUI
 import Foundation
 
 @main
+//==================STRUCT=======================
 struct Color_PaletteApp: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     
     init(){
-        print("Test")
-        fetchXMLDataFile()
+        fetchXMLDataFile() {(colorAssembled, error) in
+            if let colors = colorAssembled{
+                print("Couleurs assemblées")
+                print(colors)
+                print("Test retreving first element of array:")
+                print(colors[0])
+                print(type(of: colors[0]))
+            } else if let error = error{
+                print(error)
+            }
+        }
     }
     var body: some Scene {
         WindowGroup {
@@ -23,14 +33,136 @@ struct Color_PaletteApp: App {
     }
     
 }
+struct paletteDeCouleur{
+    //let nuance: String
+    //let couleur: String
+    let rgbValue: String //Check back to see if we change to rgb value at parsing or after
+    let hexValue: String //Check back to see if we change to hex value at parsing or after
+    let behrCode: String
+    let betonelCode: String
+    let sicoCode: String
+}
+//===============CLASS=========================
 class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool{
         
         return true
     }
 }
-
-func fetchXMLDataFile(){
+class paletteDeCouleurBuilder: NSObject, XMLParserDelegate{
+    var inNuance = false
+    var inCouleur = false
+    var inRGB = false
+    var inHEX = false
+    var inBehr = false
+    var inBetonel = false
+    var inSico = false
+    var nuanceValue: String?
+    var couleurValue: String?
+    var rgbValue: String?
+    var hexValue: String?
+    var behrCodeValue: String?
+    var betonelCodeValue: String?
+    var sicoCodeValue: String?
+    var couleursInfos = [paletteDeCouleur]()
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]){
+        switch elementName{
+        case "nuance":
+            inNuance = true
+        case "couleur":
+            inCouleur = true
+            couleurValue = ""
+        case "rgb":
+            inRGB = true
+            rgbValue = ""
+        case "hex":
+            inHEX = true
+            hexValue = ""
+        case "behr":
+            inBehr = true
+            behrCodeValue = ""
+        case "betonel":
+            inBetonel = true
+            betonelCodeValue = ""
+        case "sico":
+            inSico = true
+            sicoCodeValue = ""
+        default:
+            break
+        }
+    }
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        switch elementName {
+        case "nuance":
+            inNuance = false
+        case "couleur":
+            inCouleur = false
+            guard
+                let rgbValue = rgbValue,
+                let hexValue = hexValue,
+                let behrCodeValue = behrCodeValue,
+                let betonelCodeValue = betonelCodeValue,
+                let sicoCodeValue = sicoCodeValue
+            else {break}
+            //print(rgbValue)
+            //print(hexValue)
+            //print(behrCodeValue)
+            //print(betonelCodeValue)
+            //print(sicoCodeValue)
+            let couleurInfos = paletteDeCouleur(rgbValue: rgbValue, hexValue: hexValue, behrCode: behrCodeValue, betonelCode: betonelCodeValue, sicoCode: sicoCodeValue)
+            couleursInfos.append(couleurInfos)
+        case "rgb":
+            inRGB = false
+        case "hex":
+            inHEX = false
+        case "behr":
+            inBehr = false
+        case "betonel":
+            inBetonel = false
+        case "sico":
+            inSico = false
+        default:
+            break
+        }
+    }
+    func parser(_ parser:XMLParser, foundCharacters donnee:String){
+        if inRGB{
+            rgbValue?.append(donnee)
+        }
+        if inHEX{
+            hexValue?.append(donnee)
+        }
+        if inBehr{
+            behrCodeValue?.append(donnee)
+        }
+        if inBetonel{
+            betonelCodeValue?.append(donnee)
+        }
+        if inSico{
+            sicoCodeValue?.append(donnee)
+        }
+    }
+    func parserDidStartDocument(_ parser: XMLParser) {
+        inNuance = false
+        inCouleur = false
+        inRGB = false
+        inHEX = false
+        inBehr = false
+        inBetonel = false
+        inSico = false
+        nuanceValue = nil
+        couleurValue = nil
+        rgbValue = nil
+        hexValue = nil
+        behrCodeValue = nil
+        betonelCodeValue = nil
+        sicoCodeValue = nil
+        couleursInfos = [paletteDeCouleur]() // to check!!!!!!!!!
+        }
+}
+//================FUNCTIONS=====================
+func fetchXMLDataFile(completion: @escaping ([paletteDeCouleur]?, Error?)
+                      -> Void){
     let session = URLSession.shared
     print("Before URL")
     let url = URL(fileURLWithPath: "/Users/francisdolbec/dev/Color Palette Data/couleurs_data.xml")
@@ -38,12 +170,20 @@ func fetchXMLDataFile(){
     //Change URL depending of final destination of file!!!
     let dataTask = session.dataTask(with: url) { (data, response, error) in
         guard let data = data, error == nil else {
+            completion(nil, error)
             print("Oups!")
             print(error ?? "Response inattendu")
             return
         }
         let dataAsString = String(data: data, encoding: .utf8)!
-        print(dataAsString)
+        //print(dataAsString)
+        let parser = XMLParser(data: data)
+        let paletteBuilder = paletteDeCouleurBuilder()
+        parser.delegate = paletteBuilder
+        parser.parse()
+        let colorAssembled = paletteBuilder.couleursInfos
+        //print(colorAssembled)
+        completion(colorAssembled, nil)
     }
     dataTask.resume()
 }
